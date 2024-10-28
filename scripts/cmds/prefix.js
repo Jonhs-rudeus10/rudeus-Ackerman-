57f526b2132d151a6efdc1275803138d7423069e
Jonhs-rudeus10 <dieumerciciribagula@gmail.com>
2024-10-28 23:08:08 +0200

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
+			myPrefix: "❛ ━━━━━━･❪ ❁ ❫ ･━━━━━━ ❜                                System prefix: %1\n Your box chat prefix: %2.                                ●❯────────────────❮●"
 		}
 	},
 
